a69564ad2065f79cf834aa924574ba310f900f91
Pham Hoai <hoai04042000@gmail.com>
2021-09-13 14:52:50 +0700

    Buoi online 1 baitap 2

---
+++ BaiTap02/BaiTap02/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using BaiTap02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MyLink()
+        {
+            ViewBag.Message = "Your MyLink nha page.";
+
+            return View();
+        }
+
+        public ActionResult ViewProduct()
+        {
+            Product product = new Product();
+            product.MaSP = 10;
+            product.TenSP = "Lập trình ASP.NET MVC";
+            product.Gia = 120000;
+            product.SoLuong = 34;
+
+            ViewBag.Message = "Xem sản phẩm ";
+
+            return View(product);
+
+        }
+    }
+}
---
+++ BaiTap02/BaiTap02/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap02.Models
+{
+    public class Product
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public decimal Gia { get; set; }
+        public int SoLuong { get; set; }
+        public Product()
+        {
+                
+        }
+    }
+}
