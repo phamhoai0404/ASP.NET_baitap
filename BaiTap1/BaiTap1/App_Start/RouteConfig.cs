ed8eceb8bfd8410d4590ba3928b0a8a4feecd9a0
Pham Hoai <hoai04042000@gmail.com>
2021-09-13 12:51:53 +0700

    BT1

---
+++ BaiTap1/BaiTap1/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BaiTap1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{ID}",
+                defaults: new { controller = "Home", action = "Index", ID = UrlParameter.Optional }
+            );*/
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{ID}",
+                defaults: new { controller = "ViDu", action = "Index", ID = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ViDu",
+                url: "{controller}/{action}/{name}/{ID}"
+            );
+        }
+    }
+}
---
+++ BaiTap1/BaiTap1/Controllers/ViDuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap1.Controllers
+{
+    public class ViDuController : Controller
+    {
+        // GET: ViDu
+        public String Index()
+        {
+            return "ASP.NET đầu tiên của tôi"; 
+        }
+
+        public String Welcome()
+        {
+            return "Đây là Welcome nhá! ";
+        }
+
+        public String Welcome2(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Xin chào " + name+ ", số lần: " + numTimes);
+        }
+        public String Welcome3(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Xin chào " + name + ", ID: " + ID);
+        }
+
+
+    }
+}
