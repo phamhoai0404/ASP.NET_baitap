b9702e395de81280108a97bac128182bcfa5921e
Pham Hoai <hoai04042000@gmail.com>
2021-10-11 10:01:12 +0700

    Buoi online 5 Thuc hanh Layout

---
+++ BaiTap6/BaiTap6/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LeftSidebar()
+        {
+            return View();
+        }
+        public ActionResult RightSidebar()
+        {
+            return View();
+        }
+        public ActionResult NoSidebar()
+        {
+            return View();
+        }
+    }
+}
---
+++ BaiTap6/BaiTap6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaiTap6.MvcApplication" Language="C#" %>
